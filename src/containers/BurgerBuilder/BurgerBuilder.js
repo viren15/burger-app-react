64d8c91724b76ce9a9c8dd51963949f9148f6066
Viren Kathiriya <virenkathiriya96@gmail.com>
2020-01-07 23:51:15 +0530

    added actionCreators for fetching initIngredients // async with redux-thunk.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,23 +9,16 @@ import axios from '../../axios-orders';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component{
         state = {
-                purchasing: false,
-                loading: false,
-                error: false
+                purchasing: false
         }
 
         componentDidMount() {
                 console.log(this.props);
-                // axios.get('https://react-my-burger-faa6e.firebaseio.com/ingredients.json')
-                //         .then(response => {
-                //                 this.setState({ingredients: response.data});
-                //         }).catch(error => {
-                //                 this.setState({error: true});
-                //         });
+                this.props.onInitIngredients();
         }
 
         updatePurchaseState(ingredients) {
@@ -51,7 +44,7 @@ class BurgerBuilder extends Component{
 
         render() {
                 let orderSummary = null;
-                let burger = this.state.error ? <p style={{ color: 'red',textAlign: 'center' }}><strong>Ingredients can'tbe loaded! sorry for inconvenience</strong></p> : <Spinner />
+                let burger = this.props.error ? <p style={{ color: 'red',textAlign: 'center' }}><strong>Ingredients can'tbe loaded! sorry for inconvenience</strong></p> : <Spinner />
 
                 
                 const disableInfo = {
@@ -79,9 +72,6 @@ class BurgerBuilder extends Component{
                                 purchaseCanceled={this.purchaseCancelHandler}
                                 purchaseContinued={this.purchaseContinueHandler} />;
                 }
-                if (this.state.loading) {
-                        orderSummary = <Spinner />;
-                }
                 return (
                         <Aux>
                                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
@@ -96,14 +86,16 @@ class BurgerBuilder extends Component{
 const mapStatesToProps = state => {
         return {
                 ings: state.ingredients,
-                price: state.totalPrice
+                price: state.totalPrice,
+                error: state.error
         };
 };
 
 const mapDispatchToProps = dispatch => {
         return {
-                onIngredientAdded: (ingName) => dispatch({ type: actionTypes.ADD_INGREDIENT, ingredientName: ingName }),
-                onIngredientRemoved: (ingName) => dispatch({ type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName })
+                onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+                onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+                onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
         };
 };
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import burgerBuilder from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilder, composeEnhancers(
+	applyMiddleware(thunk)
+));
 
 const app = (
 	<Provider store={store}>
